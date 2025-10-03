d2a8646d09ec60da62285329609b64592483ba19
Biruk Debebe <bdebebe12@yahoo.com>
2025-10-03 14:06:15 +0100

    30 Day Python Chalenge
    this is the star of my 30 days python challenge to learn python apply to cyber security

---
+++ Day-01/helloworld.py
@@ -0,0 +1,24 @@
+# Introduction
+# 30 days challenge to learn python
+# this is day one 
+
+print(3 + 2) # addition(+)
+print(3 - 2) # subtraction(-)
+print(3 * 2) # multiplication(*)
+print(3 / 2) # division(/)
+print(3 ** 2) # exponential(**)
+print(3 % 2) # modulus(%)
+print(3 // 2) # Floor divison operator (//)
+
+# Checking data types
+
+print(type(8)) # Int
+print(type(3.142)) # Float
+print(type(1 + 3)) # Complex
+print(type('John')) # String
+print(type([1, 2, 3])) # List
+print(type({'name' 'John'})) # Dictionary
+print(type({1.2, 2.4, 3.6})) # Set
+print(type((1.2, 2.4, 3.6))) # Tuple
+print(type({3 == 3})) # Bool
+print(type(3 >= 3)) # Bool
